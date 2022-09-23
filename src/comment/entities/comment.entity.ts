65f17865d6f45b13239b18e6770077a849cc2719
ShuVeriDA <bashtarov@outlook.com>
2022-09-23 22:32:02 +0300

    added comment CRUD

---
+++ src/comment/entities/comment.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserEntity } from '../../user/entities/user.entity';
+import { PostEntity } from '../../post/entities/post.entity';
+
+@Entity('comments')
+export class CommentEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  text: string;
+
+  @ManyToOne(() => UserEntity, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'userId' })
+  user: UserEntity;
+
+  @ManyToOne(() => PostEntity, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'postId' })
+  post: UserEntity;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
